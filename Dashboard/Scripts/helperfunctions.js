9026b63fb8d19a3a641cda101d85a4615a1c0972
Sanket Kambli <sanketkambli09@gmail.com>
2022-07-02 22:08:01 +0530

    cards development completed

--- Dashboard/Scripts/helperfunctions.js
+++ Dashboard/Scripts/helperfunctions.js
@@ -1,46 +1,13 @@
-﻿const defaultCountrySymbol = COUNTRY_SYMBOL.India;
-const DefaultCurrency = CURRENCY.Rupees
-const defaultStyle = FORMAT.Number.Style.Numeric;
+﻿
 
-function fn_getIntlFormatter(countrySymbol, obj) {
-    if (obj) {
-        return new Intl.NumberFormat(countrySymbol, obj);
-    }
-    return new Intl.NumberFormat(countrySymbol, obj)
-}
+//let card = $('#activity_card').KPICard({
+//    data: { title: "On Time Supplies", value: 3890, KPI: [{ value: 80, className: "green-pos" }, { value: 20, className: "red-neg" }] },
+//    cardDefinition: { title: 'title', value: 'value', KPI: 'KPI' },
+//    valueOption: { applyTickerFormat: true, valueType: "number" }
+//})
 
-function fn_getCurrencyFormater(CountrySymbol, Currency) {
-    let obj = {
-        style: format.Number.Style.Currency,
-        currency: Currency
-    }
-    return fn_getIntlFormatter(CountrySymbol, obj);
-};
-
-Intl.NumberFormat.prototype.getCurrencyValue = function (val, isExcludeSymbol) {
-    try {
-        return this.format(val).substr(isExcludeSymbol ? 1 : 0, val.length);
-    }
-    catch (err) {
-        return this.format(val);
-    }
-}
-
-function fn_tickerformat(obj, val) {
-    if (obj && val && Number.isNaN(Number.parseInt(val)) == false) {
-        var formater = fn_getIntlFormatter(COUNTRY_SYMBOL.India)
-
-        let counts = setInterval(updated);
-        let upto = 0;
-
-        function updated() {
-            obj.innerHTML = formater.format(++upto);
-            if (upto >= Number.parseInt(val)) {
-                clearInterval(counts);
-            }
-        }
-    }
-    else {
-        obj.innerHTML = val;
-    }
-}
+//let Simplecard = $('#activity_card').SimpleCard({
+//    data : { title : "On Time Supplies", value : 38900 },
+//    cardDefinition : { title : 'title', value : 'value' },
+//    valueOption : { applyTickerFormat : true, valueType : "number" }
+//})
